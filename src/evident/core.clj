6eb530d1557bbe496f97757da150dcb13f0ae1dd
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2018-07-20 17:36:37 +0300

    Enable references to arguments in ctx-proj. Refactor the implementation to be minimal.

--- src/evident/core.clj
+++ src/evident/core.clj
@@ -14,97 +14,30 @@
             ast children)
     ast))
 
-(defmulti ctx-convert (fn [_ {:keys [op]}] op))
-
-(defmethod ctx-convert :fn-method [{:keys [effn? ctx-proj] :as ctx} ast]
-  (let [tmp-ctx-name (gensym 'tmp-effs)
-        tmp-ctx-atom (atom {:status :ready, :val {:tag Object}})
-        ctx (if effn?
-              {:effn?    false
-               :ctx-name (gensym 'effs)
-               :ctx-atom (atom {:status :ready, :val {:tag Object}})}
-              ctx)
-        ast (if effn?
-              (let [ctx-binding {:op        :binding
-                                 :form      tmp-ctx-name
-                                 :name      tmp-ctx-name
-                                 :local     :arg
-                                 :arg-id    0
-                                 :variadic? false
-                                 :atom      tmp-ctx-atom
-                                 :children  []}
-                    ;; (select-keys tmp-effs# ~ctx-proj)
-                    ctx-projection {:op       :invoke
-                                    :fn       {:op          :var
-                                               :form        `select-keys
-                                               :var         #'select-keys
-                                               :assignable? (not (-> #'select-keys meta :static))}
-                                    :args     [{:op          :local
-                                                :form        tmp-ctx-name
-                                                :assignable? false
-                                                :name        tmp-ctx-name
-                                                :local       :arg
-                                                :arg-id      0
-                                                :variadic?   false
-                                                :atom        tmp-ctx-atom}
-                                               (ana/analyze ctx-proj)] ;; HACK
-                                    :children [:fn :args]}]
-                (-> ast
-                    ;; ([x] ...) => ([tmp-effs# x] ...)
-                    (update :params (partial into [ctx-binding] (map #(update % :arg-id inc))))
-                    (update :fixed-arity inc)
-
-                    ;; (do ...) => (let [effs# ~ctx-projection] ...)
-                    (update :body (fn [body]
-                                    {:op         :do
-                                     :statements []
-                                     :ret        {:op       :let
-                                                  :bindings [{:op       :binding
-                                                              :form     (:ctx-name ctx)
-                                                              :name     (:ctx-name ctx)
-                                                              :local    :let
-                                                              ;; TODO: select-keys ctx-proj
-                                                              :init     ctx-projection
-                                                              :atom     (:ctx-atom ctx)
-                                                              :children [:init]}]
-                                                  :body     body
-                                                  :children [:bindings :body]}
-                                     :body?      true
-                                     :children   [:statements :ret]}))))
-              ast)]
-    (map-children (partial ctx-convert ctx) ast)))
-
-(defmethod ctx-convert :invoke [ctx ast]
-  (let [ast (if (and (= (-> ast :fn :op) :var) (= (-> ast :fn :var) #'!))
-              ;; (! :foo bar "baz") => (bar (:foo effs#) "baz")
-              (let [{callee :fn [selector action & args] :args} ast]
-                (assert selector)
-                (assert action)
-                {:op       :invoke
-                 :fn       action
-                 :args     (into [{:op       :invoke
-                                   :fn       selector
-                                   :args     [{:op          :local
-                                               :form        (:ctx-name ctx)
-                                               :assignable? false
-                                               :name        (:ctx-name ctx)
-                                               :local       :let
-                                               :atom        (:ctx-atom ctx)}]
-                                   :children [:fn :args]}]
-                                 args)
-                 :children [:fn :args]})
-              ast)]
-    (map-children (partial ctx-convert ctx) ast)))
-
-(defmethod ctx-convert :do [ctx ast] (map-children (partial ctx-convert ctx) ast))
-(defmethod ctx-convert :fn [ctx ast] (map-children (partial ctx-convert ctx) ast))
-(defmethod ctx-convert :let [ctx ast] (map-children (partial ctx-convert ctx) ast))
-
-(defmethod ctx-convert :binding [_ ast] ast)
-(defmethod ctx-convert :const [_ ast] ast)
-(defmethod ctx-convert :local [_ ast] ast)
-(defmethod ctx-convert :var [_ ast] ast)
+(defn- ctx-convert [ctx node]
+  (let [node (if (and (= (-> node :fn :op) :var) (= (-> node :fn :var) #'!))
+               ;; (! foo bar) => (foo effs# bar)
+               (let [{:keys [ctx-name ctx-atom]} ctx
+                     {[f & args] :args} node]
+                 (assert f)
+                 {:op       :invoke
+                  :fn       f
+                  :args     (into [{:op          :local
+                                    :form        ctx-name
+                                    :assignable? false
+                                    :name        ctx-name
+                                    :local       :let
+                                    :atom        ctx-atom}]
+                                  args)
+                  :children [:fn :args]})
+               node)]
+    (map-children (partial ctx-convert ctx) node)))
 
 (defmacro effn [args ctx-proj & body]
-  (->> `(fn ~args ~@body) ana/analyze (ctx-convert {:effn? true, :ctx-proj ctx-proj}) emit-form))
-
+  (let [ctx-name (gensym 'effs)]
+    (->> `(fn [tmp-ctx-name# ~@args]
+            (let [~ctx-name (select-keys tmp-ctx-name# ~ctx-proj)]
+              ~@body))
+         ana/analyze
+         (ctx-convert {:ctx-name ctx-name, :ctx-atom (atom nil)})
+         emit-form)))
